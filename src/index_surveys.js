5375288d652e605e44f5169382edb91bc52d75fb
JosePtree <jose@ptree.com.mx>
2022-04-28 13:21:35 -0500

    Comenzando creación de encuestas restantes

--- src/index_surveys.js
+++ src/index_surveys.js
@@ -1,12 +1,3 @@
-export { default as RHo } from '../src/views/pages/surveys/RHo'
-export { default as EyP } from '../src/views/pages/surveys/EyP'
-export { default as SA } from '../src/views/pages/surveys/SA'
-
-/*
-export {default as Rho} from '../src/views/pages/surveys/RHo'
-export {default as Rho} from '../src/views/pages/surveys/RHo'
-export {default as Rho} from '../src/views/pages/surveys/RHo'
-export {default as Rho} from '../src/views/pages/surveys/RHo'
-export {default as Rho} from '../src/views/pages/surveys/RHo'
-export {default as Rho} from '../src/views/pages/surveys/RHo'
-*/
+export { default as RHo } from '../src/views/components/encuestas/RHo'
+export { default as EyP } from '../src/views/components/encuestas/EyP'
+export { default as SA } from '../src/views/components/encuestas/SA'
